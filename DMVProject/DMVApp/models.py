9863f4ac3291bd425c75065dc307d3b80ff4cae5
Kenn <kenn+git@code-crew.org>
2019-10-10 15:39:52 -0500

    Fixed image URLs

--- DMVProject/DMVApp/models.py
+++ DMVProject/DMVApp/models.py
@@ -4,4 +4,4 @@ from django.db import models
 # Create your models here.
 class testModel(models.Model):
     name = models.CharField(max_length=100)
-    imageFile = models.ImageField(upload_to='images/')
+    imageFile = models.ImageField(upload_to='DMVApp/')
